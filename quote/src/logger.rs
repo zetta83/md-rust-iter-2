37f63d2fab24f37d9936ae8e73d196afa2aae6e9
zetta <azapozdaev@gmail.com>
2026-05-19 17:12:22 +0300

    fix: updated logger

--- quote/src/logger.rs
+++ quote/src/logger.rs
@@ -1,35 +1,4 @@
-#[cfg(feature = "logging")]
 pub use log::{debug, error, info, trace, warn};
-
-#[cfg(not(feature = "logging"))]
-#[macro_export]
-macro_rules! info {
-    ($($arg:tt)*) => {};
-}
-
-#[cfg(not(feature = "logging"))]
-#[macro_export]
-macro_rules! error {
-    ($($arg:tt)*) => {};
-}
-
-#[cfg(not(feature = "logging"))]
-#[macro_export]
-macro_rules! trace {
-    ($($arg:tt)*) => {};
-}
-
-#[cfg(not(feature = "logging"))]
-#[macro_export]
-macro_rules! warn {
-    ($($arg:tt)*) => {};
-}
-
-#[cfg(not(feature = "logging"))]
-#[macro_export]
-macro_rules! debug {
-    ($($arg:tt)*) => {};
-}
 
 #[cfg(feature = "logging")]
 pub fn init_logger() {
